f4fae2fff85f654c9afe43b43d7a8536e65b8f92
Xavi Torelló <info@xaviertorello.cat>
2018-04-08 19:46:16 +0200

    Rename temp container to qgis3-desktop

--- 3.0/run-qgis-3.0-in-docker.sh
+++ 3.0/run-qgis-3.0-in-docker.sh
@@ -1,9 +1,9 @@
 xhost +
 # --rm will remove the container as soon as it ends
-docker run --rm --name="qgis-desktop" \
+docker run --rm --name="qgis3.0-desktop" \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
-	XaviTorello/qgis-desktop:3.0
+	qgisdev/qgis-desktop:3.0
 xhost -
