93e6db9761169ed145246a5edbf0ea2cf723f53d
Xavi Torelló <info@xaviertorello.cat>
2018-04-09 13:39:52 +0200

    Shared host network with 3.0 container

--- 3.0/run-qgis-3.0-in-docker.sh
+++ 3.0/run-qgis-3.0-in-docker.sh
@@ -1,10 +1,10 @@
 xhost +
-# --rm will remove the container as soon as it ends
 docker run --rm --name="qgis3.0-desktop" \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
 	-v ${HOME}/qgis_plugins/python:/usr/share/qgis/python/ \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
+	--net=host \
 	qgisdev/qgis-desktop:3.0
 xhost -
