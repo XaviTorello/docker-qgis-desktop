0d2d1c6c448c140f6f1af4dd9e5a682efa0697f5
Xavi Torelló <info@xaviertorello.cat>
2018-04-09 13:39:18 +0200

    Mount shared plugins folder to 3.0

--- 3.0/run-qgis-3.0-in-docker.sh
+++ 3.0/run-qgis-3.0-in-docker.sh
@@ -3,6 +3,7 @@ xhost +
 docker run --rm --name="qgis3.0-desktop" \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
+	-v ${HOME}/qgis_plugins/python:/usr/share/qgis/python/ \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
 	qgisdev/qgis-desktop:3.0
