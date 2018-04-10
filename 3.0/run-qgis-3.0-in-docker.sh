fcc1e58e4b85b23c555ecbe7a19cc2d9c700ad15
Xavi Torelló <info@xaviertorello.cat>
2018-04-10 11:44:31 +0200

    Fix QGIS3 plugins folder overriding

--- 3.0/run-qgis-3.0-in-docker.sh
+++ 3.0/run-qgis-3.0-in-docker.sh
@@ -2,7 +2,7 @@ xhost +
 docker run --rm --name="qgis3.0-desktop" \
 	-i -t \
 	-v ${HOME}:/home/${USER} \
-	-v ${HOME}/qgis_plugins/python:/usr/share/qgis/python/ \
+	-v ${HOME}/qgis_plugins/plugins:/home/${USER}/.local/share/QGIS/QGIS3/profiles/default/python/plugins  \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-e DISPLAY=unix$DISPLAY \
 	--net=host \
